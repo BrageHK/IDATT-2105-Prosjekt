4f9c93126996c1a517c2baa22d2002a2534099f6
bragehk <bragehk@stud.ntnu.no>
2023-03-20 15:00:17 +0100

    pipe

--- Backend/src/main/java/edu/ntnu/idatt2105/backend/User.java
+++ Backend/src/main/java/edu/ntnu/idatt2105/backend/User.java
@@ -2,17 +2,26 @@ package edu.ntnu.idatt2105.backend.database;
 
 import jakarta.persistence.*;
 
-@Entity
+@Entity(name = "users")
 @Table(name = "users")
 public class User {
+
+    public User() {
+    }
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(name = "first_name", nullable = false)
     private String firstName;
+    @Column(name = "last_name", nullable = false)
     private String lastName;
+    @Column(unique = true, nullable = false, name = "email")
     private String email;
+    @Column(name = "phone_number", nullable = false)
     private Long phoneNumber;
+    @Column(name = "address", nullable = false)
     private String address;
 
 
