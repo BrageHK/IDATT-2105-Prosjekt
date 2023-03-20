e072b0094681bf27936cecd5232348473fe943e0
bragehk <bragehk@stud.ntnu.no>
2023-03-20 16:14:18 +0100

    Test now passes

--- Frontend/src/components/__tests__/Home.spec.js
+++ Frontend/src/components/__tests__/Home.spec.js
@@ -1,8 +1,9 @@
-import { shallowMount } from '@vue/test-utils';
+import { describe, expect, test } from "vitest";
+import { mount } from "@vue/test-utils";
 import Home from '@/views/HomeView.vue'; // Replace this with the correct import path to your Home component
 
 describe('Home.vue', () => {
-  it('renders a login button', () => {
+  test('renders a login button', () => {
     /*const wrapper = shallowMount(Home);
     const button = wrapper.find('button.login-button'); // Replace 'button.login-button' with the correct selector for the login button in your component
     expect(button.exists()).toBe(true);*/
