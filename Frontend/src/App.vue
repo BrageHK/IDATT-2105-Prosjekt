0c31c1e6610464f951c32a2d89f39a944d473a55
Eilert Werner Hansen <eilertwh@stud.ntnu.no>
2023-03-21 11:13:30 +0100

    Basic frontend

--- Frontend/src/App.vue
+++ Frontend/src/App.vue
@@ -1,20 +1,30 @@
-<script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import LoginComponent from './components/Login.vue';
-</script>
-
 <template>
   <header>
     <div class="wrapper">
+      <img src="@/assets/images/logo.svg" alt="My Logo" class="logo">
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/login">Login</RouterLink>
       </nav>
     </div>
   </header>
-
   <RouterView />
+  <v-footer>
+    <div class="wrapper">
+      <p>Copyright © {{ new Date().getFullYear() }} Handel uten Hemninger</p>
+    </div>
+  </v-footer>
 </template>
+
+<script setup lang="ts">
+import { RouterLink, RouterView } from 'vue-router'
+import LoginComponent from './components/Login.vue';
+import logoPath from '@/assets/images/logo.svg'
+
+const logo = logoPath
+</script>
+
+
 
 <style scoped>
 header {
@@ -24,38 +34,62 @@ header {
 
 .logo {
   display: block;
-  margin: 0 auto 2rem;
+  margin: 0 2rem 0 0;
+  height: 150px; 
+  width: auto; 
 }
 
 nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 }
 
 nav a {
   display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+  padding: 0.5rem 1rem;
+  border-radius: 0.5rem;
+  margin: 0 1rem;
+  font-size: 1.2rem;
+  font-weight: bold;
+  color: #333;
+  text-decoration: none;
+  background-color: #fff;
+  transition: all 0.2s ease;
 }
 
-nav a:first-of-type {
-  border: 0;
+nav a:hover,
+nav a.router-link-exact-active {
+  background-color: #0718c4;
+  color: #fff;
 }
+
+v-footer {
+    background-color: #0718c4;
+    color: white;
+    height: 100px;
+    display: flex;
+    align-items: center;
+    margin-top: auto;
+  }
+
+  v-footer .wrapper {
+    max-width: 1200px;
+    margin: 0 auto;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+  }
+
+  v-footer p {
+    margin: 0;
+    font-size: 1.2rem;
+  }
 
 @media (min-width: 1024px) {
   header {
     display: flex;
-    place-items: center;
+    align-items: center;
     padding-right: calc(var(--section-gap) / 2);
   }
 
@@ -65,17 +99,14 @@ nav a:first-of-type {
 
   header .wrapper {
     display: flex;
-    place-items: flex-start;
+    align-items: center;
+    justify-content: space-between;
     flex-wrap: wrap;
   }
 
   nav {
-    text-align: left;
-    margin-left: -1rem;
+    margin-left: auto;
     font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
   }
 }
 </style>
--- Frontend/src/views/HomeView.vue
+++ Frontend/src/views/HomeView.vue
@@ -1,12 +1,68 @@
 <script setup lang="ts">
+import ShoppingListing from '@/components/Listing.vue';
 </script>
 
 <template>
   <main>
-    <p>Hei</p>
-
-
-    <!-- <Scrollable, paginated, and thumbnail view /> -->
-    
+    <div class="grid">
+      <ShoppingListing v-for="id in 20" :id="id" />
+    </div>
   </main>
 </template>
+
+<style scoped>
+
+.grid {
+  display: grid;
+  grid-template-columns: repeat(5, minmax(calc(20% - 1rem), 1fr));
+  grid-gap: 1rem;
+  margin-left: auto; 
+  margin-right: auto; 
+  max-width: 1524px;
+}
+
+
+
+@media screen and (min-width: 480px) {
+  .grid {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+}
+
+@media screen and (max-width: 480px) {
+  .grid {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+}
+
+@media screen and (min-width: 640px) {
+  .grid {
+    grid-template-columns: repeat(2, minmax(0, 1fr));
+  }
+}
+
+@media screen and (min-width: 768px) {
+  .grid {
+    grid-template-columns: repeat(2, minmax(calc(50% - 0.5rem), 1fr));
+  }
+}
+
+@media screen and (min-width: 1024px) {
+  .grid {
+    grid-template-columns: repeat(3, minmax(calc(33.33% - 0.666rem), 1fr));
+  }
+}
+
+@media screen and (min-width: 1200px) {
+  .grid {
+    grid-template-columns: repeat(4, minmax(calc(25% - 0.75rem), 1fr));
+  }
+}
+
+@media screen and (min-width: 1524px) {
+  .grid {
+    grid-template-columns: repeat(5, minmax(calc(20% - 1rem), 1fr));
+  }
+}
+
+</style>
